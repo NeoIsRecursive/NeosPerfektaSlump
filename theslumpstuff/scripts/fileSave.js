e58eaa0cfc196d2d2b39c1a8795a8c5cc56fa7ca
NeoIsRecursive <64473191+NeoIsRecursive@users.noreply.github.com>
2022-02-07 18:57:22 +0100

    2.0 init

---
+++ theslumpstuff/scripts/fileSave.js
@@ -0,0 +1,14 @@
+    //denna funktionen skapar en fil med grupperna som användaren kan ladda ner
+    //skapar en "blob" med text som sedan kan laddas ner, hur vet jag inte riktigt då det inte fanns super mycket på nätet om det :/    
+    function downloadGroup(x,groups,count){
+        test = document.getElementById('file').files[0];
+        var y;
+        if(test == null){
+            y = "min_lista";
+        }else {
+            y = test.name.split(".")[0];
+        }
+        let blob = new Blob([groups.join("\n")], {type:'text/html'});
+        x.setAttribute("href",URL.createObjectURL(blob));
+        x.setAttribute("download",count +" grupper_i_"+y+".txt");
+    }
---
+++ theslumpstuff/scripts/groups.js
@@ -0,0 +1,68 @@
+	//vet inte rikitgt varför den ligger här, men växlar mellan grupper och slump av students
+	function show() {
+        var x = document.getElementById('container');
+        var y = document.getElementById('grupper');
+        if (x.style.display === 'none') {
+            x.style.display = 'flex';
+            y.style.display = 'none';
+			document.getElementById('showbtn').innerHTML = "Groups";
+        } else {
+            x.style.display = 'none';
+            y.style.display = 'block';
+			document.getElementById('showbtn').innerHTML = "Questions";
+        }
+    }
+	
+	
+	//skapar grupperna
+	function createGroups(x) {
+		if (x <= 0){
+	        alert('Do you really need 0 groups?');
+	        return;
+	    } else if (x == students.length){
+	        alert("there is literally no point bro");
+	        return;
+	    } else if (x > students.length){
+	        alert("try fewer groups");
+	        return; 
+		}
+        var groups = [];
+		var noGroup = [...students];
+		var antal = Math.floor(students.length / x);
+		var iGrupp = 0;
+		
+		for(var i = 0; i < x; i++){
+		    groups.push([]);
+		    groups[i].length = antal;
+		    iGrupp += antal;
+		}
+
+		for (i = 0; i < x; i++) {
+			for (var y = 0; y < groups[i].length;y++){
+				raNum = randFloor(noGroup.length);
+				groups[i][y] = (noGroup[raNum][0]);
+				noGroup.splice(raNum,1);
+			}
+		}
+		
+		if (noGroup.length > 0){
+		    let kvar = noGroup.length;
+		    while(noGroup.length > 0){
+				for (i = 0; i < kvar; i++) {
+					raNum = randFloor(noGroup.length)
+					groups[i].push(noGroup[raNum][0]);
+					noGroup.splice(raNum,1)
+				}
+		    }
+		}
+        drawGroups(groups);
+	}
+    
+
+	function copyGroups(groups = document.getElementById('groups')){
+		console.log(groups.innerText);
+		navigator.clipboard.writeText(groups.innerText);
+		if (groups.id == "groups") {console.log("copied all") }else {
+			console.log("copied "+groups.id);
+		};
+	}
