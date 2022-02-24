3107d472f89f38468fa3b1cc52297c694ac833d5
NeoIsRecursive <64473191+NeoIsRecursive@users.noreply.github.com>
2022-02-24 15:47:57 +0100

    favicon loads by itself?

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -4,7 +4,7 @@
 <head>
     <title>Neos perfekta slump</title>
     {{-- <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}"> --}}
-    <link rel="icon" type="image/x-icon" href="{{ asset('images/favicon.ico') }}">
+    {{-- <link rel="icon" type="image/x-icon" href="{{ asset('images/favicon.ico') }}"> --}}
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="UTF-8">
     @vite
