2e7e0db2b0b8812f982485c0010b143ed63e8d70
NeoIsRecursive <64473191+NeoIsRecursive@users.noreply.github.com>
2022-03-04 10:51:24 +0100

    node version specified

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
 module.exports = {
-	content: [
-		'./resources/**/*.blade.php',
-		'./resources/**/*.js',
-	],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+    content: ["./resources/**/*.blade.php", "./resources/**/*.js"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
