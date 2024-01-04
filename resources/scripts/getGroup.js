e0e264c34aef4306779b57a71b96b9eff75502b0
neoisrecursive <neo.leijondahl@gmail.com>
2024-01-04 23:13:57 +0100

    init 3.0

--- resources/scripts/getGroup.js
+++
@@ -1,9 +0,0 @@
-import load from "./load.js";
-
-export async function getGroup(id) {
-    load(true);
-    const data = await fetch("getGroupApi?id=" + id);
-    const groupContent = await data.json();
-    load(false);
-    asker.changeList(groupContent);
-}
--- resources/views/components/error.blade.php
+++
@@ -1,5 +0,0 @@
-@if ($errors->any())
-    <div class="bg-red-400 px-4 py-2 rounded">
-        <p class="text-center">{{ $errors->first() }}</p>
-    </div>
-@endif
