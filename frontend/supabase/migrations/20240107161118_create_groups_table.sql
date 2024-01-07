f9e5d71daca8287c5ca91d37d4e061556eeb1683
neoisrecursive <neo.leijondahl@gmail.com>
2024-01-07 17:18:25 +0100

    Add members and groups tables

---
+++ frontend/supabase/migrations/20240107161118_create_groups_table.sql
@@ -0,0 +1,57 @@
+create table "public"."groups" (
+    "id" uuid not null default gen_random_uuid(),
+    "created_at" timestamp with time zone not null default now(),
+    "name" text not null,
+    "owned_by" uuid default auth.uid()
+);
+
+
+alter table "public"."groups" enable row level security;
+
+CREATE UNIQUE INDEX groups_pkey ON public.groups USING btree (id);
+
+alter table "public"."groups" add constraint "groups_pkey" PRIMARY KEY using index "groups_pkey";
+
+grant delete on table "public"."groups" to "anon";
+
+grant insert on table "public"."groups" to "anon";
+
+grant references on table "public"."groups" to "anon";
+
+grant select on table "public"."groups" to "anon";
+
+grant trigger on table "public"."groups" to "anon";
+
+grant truncate on table "public"."groups" to "anon";
+
+grant update on table "public"."groups" to "anon";
+
+grant delete on table "public"."groups" to "authenticated";
+
+grant insert on table "public"."groups" to "authenticated";
+
+grant references on table "public"."groups" to "authenticated";
+
+grant select on table "public"."groups" to "authenticated";
+
+grant trigger on table "public"."groups" to "authenticated";
+
+grant truncate on table "public"."groups" to "authenticated";
+
+grant update on table "public"."groups" to "authenticated";
+
+grant delete on table "public"."groups" to "service_role";
+
+grant insert on table "public"."groups" to "service_role";
+
+grant references on table "public"."groups" to "service_role";
+
+grant select on table "public"."groups" to "service_role";
+
+grant trigger on table "public"."groups" to "service_role";
+
+grant truncate on table "public"."groups" to "service_role";
+
+grant update on table "public"."groups" to "service_role";
+
+
